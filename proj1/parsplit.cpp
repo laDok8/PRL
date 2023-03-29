b94334f563118fc883deb66f3067665ca4072465
Ladislav Dokoupil <ldokoupi@redhat.com>
2023-03-29 08:53:54 +0200

    move

--- proj1/parsplit.cpp
+++ proj1/parsplit.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    int rank, size;
+    int rank=0, size=0;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
